32ceed143d4cbc8ae091ab2039872cb59650cb17
InfiniteBuffe <parktaejin@ptj.kr>
2021-01-22 06:54:17 +0900

    Input component에 aos fade up 효과 추가

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -5,8 +5,13 @@ const Input = (props) => {
     return (
         <>
             <div className="Container">
-                <div className="Input__box">
-                    <input className="Input" placeholder={props.placeholder} />
+                <div
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                >
+                    <div className="Input__box">
+                        <input className="Input" placeholder={props.placeholder} />
+                    </div>
                 </div>
             </div>
         </>
